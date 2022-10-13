edb6b13e18f7bf825b07780f454a54be2cc02fe8
Riya.R <riyaramteke55@gmail.com>
2022-10-13 11:24:23 +0000

    new data is added in issuefix.java

--- issuefix.java
+++ issuefix.java
@@ -1,1 +1,5 @@
 this code is added to fix issue in issue1 branch by dev 1 and it will be merge in master branch
+now its in the master
+
+not again the code is modified in issue2 by dev2
+new line is added in the code to create conglict
